cd9e371f5e4e2d1bee7c50253c38c7d60a6801ae
Richard Axornam <axornam.richard@gmail.com>
2021-02-19 16:04:46 +0000

    Add different navs for side drawer

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,11 +3,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   const navBar = document.querySelector(".navbar");
   const mainSideNav = document.querySelector(".side-nav-drawer");
-  const navButton = document.getElementById("side-nav-btn");
-  const overlay = document.querySelector(".overlay");
-  const mainSideNavBackButton = document.querySelector(
-    ".sn__Header_Buttons span:first-child"
+  const mainSideNavButtons = document.querySelectorAll(".sn__HI_Buttons");
+  const mainNavContent = document.querySelector("#main-nav-content");
+  const productsNavContent = document.querySelector("#products-nav-content");
+  const resourcesNavContent = document.querySelector("#resources-nav-content");
+  const inspirationNavContent = document.querySelector(
+    "#inspiration-nav-content"
   );
+
+  const overlay = document.querySelector(".overlay");
+  const navButton = document.getElementById("side-nav-btn");
+  const mainSideNavBackButton = document.querySelectorAll(".sn__Back_Btn");
 
   const showHideMainSideNav = () => {
     // show - hide the side drawer
@@ -21,12 +27,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   };
 
+  const showSideMenu = (body) => {
+    mainSideNav.querySelector(".sn__Content").innerHTML = body.innerHTML;
+    // console.log(body.innerHTML);
+  };
+
   navBar.addEventListener(
     "mouseenter",
     () => (navBar.style.backgroundColor = "#fff")
   );
 
+  // ADD EVENT LISTENER TO NAV BUTTON
   navButton.addEventListener("click", showHideMainSideNav);
+
+  // ADD OVERLAY EVENT LISTENER
   overlay.addEventListener("click", showHideMainSideNav);
-  mainSideNavBackButton.addEventListener("click", showHideMainSideNav);
+  mainSideNavBackButton.forEach((nodeItem) =>
+    nodeItem.addEventListener("click", showHideMainSideNav)
+  );
+
+  // ADD EVENT LISTENERST TO MAIN SIDE NAV BUTTONS
+  mainSideNavButtons.forEach((nodeItem) =>
+    nodeItem.addEventListener("click", () => {
+      if (nodeItem.innerText === "Pricing") {
+      } else if (nodeItem.innerText === "Inspiration") {
+        showSideMenu(inspirationNavContent);
+      } else if (nodeItem.innerText === "Resources") {
+        showSideMenu(resourcesNavContent);
+      } else if (nodeItem.innerText === "Products") {
+        showSideMenu(productsNavContent);
+      } else {
+        console.log("invalid parameter item");
+      }
+      console.log("NODE ITEM: ", nodeItem.innerText);
+    })
+  );
 });
